3b93af1c75e0a1f2a5c098ad0920fffcb0ec7464
Obi Ngozi Elizabeth <elizabethngozi08@gmail.com>
2026-05-23 14:45:25 +0100

    finishesd corrections and work

--- javascript/01a-intro/01a-intro.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-console.log("this is an external Javascript file.")
-=======
-console.log("This is an external JavaScript file.");
->>>>>>> 089292008401f3736851002873f61f5c0ece8ffb
--- javascript/01a-intro/01a-standalone.js
+++
@@ -1,2 +0,0 @@
-console.log("This is a standalone JavaScript Executable.");
-console.log("This is a standalone Javascript Executable.");
