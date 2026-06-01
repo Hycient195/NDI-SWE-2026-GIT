035bad0a68de6d8782801609945627ad10f5c9b9
Miesintei Mark Osaisai <osaisaimark@gmail.com>
2026-06-01 16:08:56 +0100

    Made corrections to function and callback assignment

--- assignments/06.function-and-callback/mark/function-and-callback-assignment.js
+++ assignments/06.function-and-callback/mark/function-and-callback-assignment.js
@@ -1,31 +1,11 @@
-// const celsiusDeg = 32;
-// function multiply(a, b){
-//     return a * b;
-// }
-
-// function add(a, b){
-//     return a + b;
-// }
-
-
-// const multiplicationResult = multiply(celsiusDeg, 1.8);
-// const additionResult = add(multiplicationResult,32);
-// console.log(celsiusDeg, "in Celsius is", additionResult, "in Farenheit");
-
 function calc(tempInput, callback){
     const result = ((tempInput * 1.8)+32);
     callback(tempInput, result);
 }
 function tempConversion(tempInput, result){
-    console.log(tempInput, "in Celsius is", result, "in Farenheit");
-
-// const tempConverter = function(result){
-//     console.log(celsiusDeg, "in Celsius is", result, "in Farenheit");
-
-// const tempConverter = (result)=>{
-//     console.log(celsiusDeg, "in Celsius is", result, "in Farenheit");    
+    console.log(tempInput, "in Celsius is", result, "in Farenheit");   
 
 }
-calc(52, tempConversion )
+calc(65, tempConversion );
 
 
