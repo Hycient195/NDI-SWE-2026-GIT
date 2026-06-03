8c6204273ddd3b4d54952eb4783ac45e8752df4e
Miesintei Mark Osaisai <osaisaimark@gmail.com>
2026-06-03 19:11:34 +0100

    Added 08-string-assignment/update/mark

--- assignments/08-string/mark/o8-string-assignment.js
+++ assignments/08-string/mark/o8-string-assignment.js
@@ -1,12 +1,13 @@
 // QUESTION ONE
-const maskEmail = function theMask (email){
-    const visible = email.slice(0, 4);
-    const visible2 = email.slice(-10)
-    const asterixLength = (email.length -14);
-    return visible + "*".repeat(asterixLength) + visible2
-}
+const maskEmail = function theMask(email) {
+  const atIndex = email.indexOf("@");
+  const visible = email.slice(0, 4);
+  const visible2 = email.slice(atIndex);
+  const asterixLength = atIndex - 4;
+  return visible + "*".repeat(asterixLength) + visible2;
+};
 
-console.log(maskEmail("miesinteiosaisai@gmail.com"));
+console.log(maskEmail("Abujatravels@gmail.com"));
 
 
 
