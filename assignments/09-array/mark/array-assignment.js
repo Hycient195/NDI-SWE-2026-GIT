737625b02405d49e7123f1b333f765e6d68dddf0
Miesintei Mark Osaisai <osaisaimark@gmail.com>
2026-06-04 05:55:37 +0100

    Added question-two-of-09-arry-assigment/mark

--- assignments/09-array/mark/array-assignment.js
+++ assignments/09-array/mark/array-assignment.js
@@ -10,7 +10,26 @@ const myCartBill = myCart.map(priQuan => {
     return priQuan.price * priQuan.quantity;
 });
 const totalBill = myCartBill.reduce((acc, curr) => acc + curr, 0);
-console.log(totalBill); 
+console.log("N" + totalBill ); 
 
 
+
+// QUESTION TWO
+const theNumbers = [2, 5, 7, 1, 1, 9, 5];
+
+const countOccurrences = (theNumber) => {
+    const result = {};
+
+    theNumber.forEach((value) => {
+        if (result[value]) {
+            result[value] += 1;
+        } else {
+            result[value] = 1;
+        }
+    });
+
+    return result;
+};
+
+console.log(countOccurrences(theNumbers));
 
