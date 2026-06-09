c45296d53d8312e574bfbf201464bb3163347dd5
Ephraim Samuel <ephraimsamuel036@gmail.com>
2026-06-09 10:12:07 +0100

    add all new content

--- assignments/10-Math/010-maths.assignment.js
+++ assignments/10-Math/010-maths.assignment.js
@@ -2,11 +2,6 @@
 Write a function that takes in two numbers, min and max, and generates a random number between
 the min and max numbers, with both min and max being in the range of the nubers generated
 */
-const max = Math.max(3,2,5,1,7,8,6)
-console.log("Max", max)
-
-const min = Math.min(3,2,5,1,7,8,6)
-console.log("Min", min)
 
 
 function generateRandomNumber(min, max) {
